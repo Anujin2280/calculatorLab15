ea1c339ca5987006e221c7ba0f98e584a237a30c
“Temuujin” <temuujinbatdorj@gmail.com>
2025-05-09 16:20:00 +0800

    Multiplication функц болон JUnit тест нэмсэн

---
+++ src/main/java/labxx/sict/must/edu/mn/Multiplication.java
@@ -0,0 +1,8 @@
+package labxx.sict.must.edu.mn;
+
+public class Multiplication {
+    public double multiply(double a, double b) {
+        if (a == 0 || b == 0) return 0;
+        return a * b;
+    }
+}
---
+++ src/test/java/labxx/sict/must/edu/mn/MultiplicationTest.java
@@ -0,0 +1,20 @@
+package labxx.sict.must.edu.mn;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class MultiplicationTest {
+
+    Multiplication mul = new Multiplication();
+
+    @Test
+    void testMultiplyPositive() {
+        assertEquals(20.0, mul.multiply(4, 5));
+    }
+
+    @Test
+    void testMultiplyByZero() {
+        assertEquals(0.0, mul.multiply(4, 0));
+        assertEquals(0.0, mul.multiply(0, 100));
+    }
+}
